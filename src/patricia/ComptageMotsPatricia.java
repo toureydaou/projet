962601db480cc33b3ddc13c24f3c5591606fa751
toureydaou <ttoureydaou@gmail.com>
2024-12-15 21:11:08 +0100

    script bash comptageMots arbre patricia

---
+++ src/patricia/ComptageMotsPatricia.java
@@ -0,0 +1,44 @@
+package patricia;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class ComptageMotsPatricia {
+    public static void comptageMots(String file) {
+        try {
+            // instanciation d'un arbre patrica via le fichier json
+            Gson gson = new GsonBuilder().registerTypeAdapter(Patricia.class, new PatricaJsonDeserializer()).create();
+            String patriciaJson = new String(Files.readAllBytes(Paths.get(file)));
+            Patricia arbrePatricia = gson.fromJson(patriciaJson, Patricia.class);
+
+            int nombreMots = Patricia.comptageMots(arbrePatricia);
+
+            // écriture de la hauteur dans le fichier nombreNil.txt
+            File fichierNombreMots = new File("nombreMots.txt");
+            if (fichierNombreMots.exists()) {
+                fichierNombreMots.delete();
+                fichierNombreMots.createNewFile();
+            } else {
+                fichierNombreMots.createNewFile();
+            }
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fichierNombreMots));
+
+            writer.write(nombreMots + "\n");
+
+            writer.close();
+
+            System.out
+                    .println("Nombre de mots dans l'arbre sauvegardé dans le fichier "
+                            + fichierNombreMots.getPath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
